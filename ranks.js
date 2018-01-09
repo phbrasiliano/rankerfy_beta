1effc94c950b938bfdbcc1e07390738d44220571
Phellipe <phbrasiliano@gmail.com>
2018-01-10 00:53:37 +0100

    adds ranks.js file with the lists to be ranked on the app

---
+++ ranks.js
@@ -0,0 +1,14 @@
+var iceCreamJson = {
+  "title": "Ice Cream Flavors",
+  "explanation": "Those are the most popular Ice Cream Flavors according to the International Ice Cream Association. From those flavors, which ones are YOUR favorites?",
+  "list": { "Vanilla" :{ "img": "http://www.tidyaire.com/uploads/7/4/2/9/74291919/s677363335125410898_p12_i1_w500.jpeg"},
+            "Chocolate" :{ "img": "http://www.milkmaid.in/Images/Recipe/Chocolate%20694x400_11.JPG"},
+            "Strawberry" :{ "img": "http://holisticallyengineered.com/wp-content/uploads/2012/09/strawberry-ice-cream_tuneblog-1.jpg"},
+            "Neapolitan" :{ "img": "http://www.articlesweb.org/blog/wp-content/uploads/2011/11/neapolitan-ice-cream-origin-and-recipe-sources-4.jpg"},
+            "Chocolate chip" :{ "img": "http://www.vegrecipesofindia.com/wp-content/uploads/2016/12/chocolate-chips-ice-cream-recipe-12.jpg"},
+            "Cookies and Cream" :{ "img": "http://www.chewoutloud.com/wp-content/uploads/2013/08/Cookies-n-Cream-Ice-Cream-2.jpg"},
+            "Cherry" :{ "img": "http://www.seriouseats.com/recipes/assets_c/2012/06/20120627-cherry-ice-cream-primary-thumb-625xauto-252967.jpg"},
+            "Chocolate almond" :{ "img": "https://barefeetinthekitchen.com/wp-content/uploads/2013/07/chocolate-almond-ice-cream-8.jpg"},
+            "Pistachio" :{ "img": "https://www.tablefortwoblog.com/wp-content/uploads/2014/04/pistachio-ice-cream-tablefortwoblog-2.jpg"},
+            "Mint Chocolate" :{ "img": "http://www.recipe4living.com/assets/itemimages/400/400/3/default_36fc5210cd4f4bae865ead91bef9071c_junemediamint.jpg"}}
+}
